12a3419326031d38bec9c8d53226dc6aa3e109db
Glad Chinda <gladxeqs@gmail.com>
2018-04-15 17:29:25 +0100

    Removed old files

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Rave\\' => array($vendorDir . '/gladcodes/ravephp/src/Rave'),
+    'Dotenv\\' => array($vendorDir . '/vlucas/phpdotenv/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,28 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit5b9369fcf0fff595cc17ec4907376867
 {
+    public static $prefixLengthsPsr4 = array (
+        'R' => 
+        array (
+            'Rave\\' => 5,
+        ),
+        'D' => 
+        array (
+            'Dotenv\\' => 7,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Rave\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/gladcodes/ravephp/src/Rave',
+        ),
+        'Dotenv\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/vlucas/phpdotenv/src',
+        ),
+    );
+
     public static $prefixesPsr0 = array (
         'U' => 
         array (
@@ -26,6 +48,8 @@ class ComposerStaticInit5b9369fcf0fff595cc17ec4907376867
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit5b9369fcf0fff595cc17ec4907376867::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit5b9369fcf0fff595cc17ec4907376867::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit5b9369fcf0fff595cc17ec4907376867::$prefixesPsr0;
 
         }, null, ClassLoader::class);
